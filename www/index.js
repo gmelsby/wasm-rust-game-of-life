f505203934a39b6bd2cbb3233972a9e26b9297c4
Gregory Melsby <greg@gregexp.com>
2024-06-12 16:14:08 -0400

    mass death happens on first tick

--- www/index.js
+++ www/index.js
@@ -1,3 +1,18 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("World");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+pre.textContent = universe.render();
+
+/*
+const loop = () => {
+  console.log('going');
+  pre.textContent = universe.render();
+  universe.tick();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+*/
