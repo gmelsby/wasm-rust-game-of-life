e1e6653338418a2ba6beeb3a4960bcc84709570c
Gregory Melsby <greg27426@gmail.com>
2024-06-12 16:17:40 -0400

    mass death event caused by initial state

--- www/index.js
+++ www/index.js
@@ -3,16 +3,10 @@ import { Universe } from "wasm-game-of-life";
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
-pre.textContent = universe.render();
-
-/*
 const loop = () => {
-  console.log('going');
   pre.textContent = universe.render();
   universe.tick();
   requestAnimationFrame(loop);
 }
 
 requestAnimationFrame(loop);
-
-*/
